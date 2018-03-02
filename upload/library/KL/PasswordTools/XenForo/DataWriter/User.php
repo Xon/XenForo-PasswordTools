81d595a0a44381dcbc20bd2cbf915becbb562e57
Xon <tazipper1@hotmail.com>
2018-03-03 04:01:14 +0800

    Sure rought pwned password check

--- upload/library/KL/PasswordTools/XenForo/DataWriter/User.php
+++ upload/library/KL/PasswordTools/XenForo/DataWriter/User.php
@@ -1,12 +1,5 @@
 <?php
 
-/**
- * KL_PasswordTools_XenForo_DataWriter_User
- *
- *	@author: Katsulynx
- *  @last_edit:	21.08.2015
- */
-
 class KL_PasswordTools_XenForo_DataWriter_User extends XFCP_KL_PasswordTools_XenForo_DataWriter_User
 {
     public function setPassword($password, $passwordConfirm = false, XenForo_Authentication_Abstract $auth = null, $requirePassword = false)
@@ -20,54 +13,121 @@ class KL_PasswordTools_XenForo_DataWriter_User extends XFCP_KL_PasswordTools_Xen
 
         if ($requirePassword)
         {
-            /* Gather required stuff */
-            $pattern = $password;
-            $zxcvbn = new KL_PasswordTools_Zxcvbn();
-
-
-            /* Gather requirements */
-            $requirements = [
-                'minimum_score'  => $options->KL_PasswordStrengthMeter_str,
-                'minimum_length' => $options->KL_PasswordStrengthMeter_min,
-                'force_reject'   => $options->KL_PasswordStrengthMeter_force,
-                'blacklist'      => array_merge($options->KL_PasswordStrengthMeter_blacklist, [$options->boardTitle])
-            ];
-
-            /* Check against length */
-            if (strlen($pattern) < $requirements['minimum_length'])
+            if (!empty($options->passwordToolsCheckTypes['zxcvbn']) && !$this->sv_zxcvbnCheck($password))
             {
-                $this->error(new XenForo_Phrase('KL_PasswordStrengthMeter_error_TooShort'), 'password');
-
                 return false;
             }
-
-            /* Run Zxcvbn */
-            $pwd_result = $zxcvbn->passwordStrength($pattern, $requirements['blacklist']);
-
-            /* Check against score */
-            if ($pwd_result['score'] < $requirements['minimum_score'])
+            if (!empty($options->passwordToolsCheckTypes['pwned']) && !$this->sv_pwnedCheck($password))
             {
-                $this->error(new XenForo_Phrase('KL_PasswordStrengthMeter_error_TooWeak'), 'password');
-
                 return false;
-            }
-
-            /* Check for blacklist hit with force reject enabled */
-            if ($requirements['force_reject'])
-            {
-                foreach ($pwd_result['match_sequence'] as $match_sequence)
-                {
-                    if (isset($match_sequence->dictionaryName) && $match_sequence->dictionaryName === 'user_inputs')
-                    {
-                        $this->error(new XenForo_Phrase('KL_PasswordStrengthMeter_errorInvalidExpression'), 'password');
-
-                        return false;
-                    }
-                }
             }
         }
 
         return parent::setPassword($password, $passwordConfirm, $auth, $requirePassword);
+    }
+
+    protected function sv_zxcvbnCheck($password)
+    {
+        /* Gather required stuff */
+        $pattern = $password;
+        $zxcvbn = new KL_PasswordTools_Zxcvbn();
+
+        /* Gather requirements */
+        $options = XenForo_Application::getOptions();
+        $requirements = [
+            'minimum_score'  => $options->KL_PasswordStrengthMeter_str,
+            'minimum_length' => $options->KL_PasswordStrengthMeter_min,
+            'force_reject'   => $options->KL_PasswordStrengthMeter_force,
+            'blacklist'      => array_merge($options->KL_PasswordStrengthMeter_blacklist, [$options->boardTitle])
+        ];
+
+        /* Check against length */
+        if (strlen($pattern) < $requirements['minimum_length'])
+        {
+            $this->error(new XenForo_Phrase('KL_PasswordStrengthMeter_error_TooShort'), 'password');
+
+            return false;
+        }
+
+        /* Run Zxcvbn */
+        $pwd_result = $zxcvbn->passwordStrength($pattern, $requirements['blacklist']);
+
+        /* Check against score */
+        if ($pwd_result['score'] < $requirements['minimum_score'])
+        {
+            $this->error(new XenForo_Phrase('KL_PasswordStrengthMeter_error_TooWeak'), 'password');
+
+            return false;
+        }
+
+        /* Check for blacklist hit with force reject enabled */
+        if ($requirements['force_reject'])
+        {
+            foreach ($pwd_result['match_sequence'] as $match_sequence)
+            {
+                if (isset($match_sequence->dictionaryName) && $match_sequence->dictionaryName === 'user_inputs')
+                {
+                    $this->error(new XenForo_Phrase('KL_PasswordStrengthMeter_errorInvalidExpression'), 'password');
+
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public function sv_pwnedCheck($password)
+    {
+        $options = XenForo_Application::getOptions();
+        $minimumUsages = intval($options->pwnedPasswordReuseCount);
+
+        $hash = strtoupper(sha1($password));
+        $prefix = substr($hash, 0, 5);
+        $suffix = substr($hash, 5);
+        $suffixSet = $this->getPwnedPrefixMatches($prefix);
+        if ($suffixSet === null || $suffixSet === false)
+        {
+            return true;
+        }
+        if (isset($suffixSet[$suffix]) && $suffixSet[$suffix] >= $minimumUsages)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function getPwnedPrefixMatches($prefix)
+    {
+        $suffixCount = [];
+        try
+        {
+            $options = XenForo_Application::getOptions();
+            $httpClient = XenForo_Helper_Http::getClient("https://api.haveibeenpwned.com/v2/range/{$prefix}", [
+                'useragent' => "XenForo/" . XenForo_Application::$version . " (" . $options->boardUrl . ')',
+            ]);
+            $response = $httpClient->request('GET');
+            if ($response->getStatus() == 200)
+            {
+                $text = $response->getBody();
+                $suffixSet = array_filter(array_map('trim', explode("\n", $text)));
+
+                foreach ($suffixSet as $suffix)
+                {
+                    $suffixInfo = explode(':', trim($suffix));
+                    $suffixCount[$suffixInfo[0]] = intval($suffixInfo[1]);
+                }
+            }
+        }
+        catch (Exception $e)
+        {
+            XenForo_Error::logException($e, false);
+
+            return null;
+        }
+
+        return $suffixCount;
     }
 }
 
